a83d1cca453f4fb4d76001360463619f03c9af9f
Priyotush Paul <priyotushpaul40@gmail.com>
2025-08-16 01:58:21 +0600

    Update problem1.js

--- problem1.js
+++ problem1.js
@@ -1,13 +1,9 @@
 function totalFine(fare) {
-  if (typeof fare !== "number" || !fare > 0) {
+  if (typeof fare !== "number" || fare <= 0) {
     return "Invalid";
   }
 
   let fine = fare + (fare * 20) / 100 + 30;
-
-  if (fine < 0) {
-    return "Invalid";
-  }
 
   return fine;
 }
